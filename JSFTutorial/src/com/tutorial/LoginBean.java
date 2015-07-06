9b8fa7ef213c08914992da39e18245f58edaba34
dave45678 <dave45678@gmail.com>
2015-07-06 16:45:53 -0400

    First Commit

---
+++ JSFTutorial/src/com/tutorial/LoginBean.java
@@ -0,0 +1,24 @@
+package com.tutorial;
+
+public class LoginBean {
+
+	private String name;
+	private String password;
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String value)
+	{
+		name = value;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(final String value)
+	{
+		password = value;
+	}
+}
